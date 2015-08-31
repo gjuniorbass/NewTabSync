cd29d3a6bbe9d077c3fca4688cfa1cabb69875fe
gjuniorbass <gjuniorbass@hotmail.com>
2015-08-30 23:51:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/gjunior/newtabsync/menu2_Fragment.java
@@ -0,0 +1,21 @@
+package com.example.gjunior.newtabsync;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gjuni on 30/08/2015.
+ */
+public class menu2_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview=inflater.inflate(R.layout.menu2_layout, container, false);
+        return rootview;
+    }
+}
